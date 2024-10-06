5d61a00d2de4f931c1e8d415a56e947bc3e844a2
Shiva Saini <factwiths@gmail.com>
2024-10-06 16:25:13 +0530

    removing learning/page.tsx

--- src/app/learning/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import dynamic from 'next/dynamic'
-
-const Learning = dynamic(() => import('@/components/Learning/Learning'), { ssr: false })
-
-export default function LearningPage() {
-  return <Learning />
-}
